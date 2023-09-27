e1a04382839bc8f1604593a1fc9835d22d9d438a
indahouse-aleksandr <indahouse.aleksandr@gmail.com>
2023-09-27 19:50:23 +0300

    fix claim model

--- models/claim.go
+++ models/claim.go
@@ -2,11 +2,10 @@ package models
 
 import (
 	"github.com/golang-jwt/jwt"
-	"github.com/google/uuid"
 )
 
 type Claims struct {
-	UserID   uuid.UUID `json:"user_id"`
-	Username string    `json:"username"`
+	UserID   int32  `json:"user_id"`
+	Username string `json:"username"`
 	jwt.StandardClaims
 }
