debc8bc529796c9b00dd793f6da96ff1e78ca0eb
cr1m3s <parioleks@gmail.com>
2023-10-05 13:11:17 +0200

    moved server setup into routers package

--- main.go
+++ main.go
@@ -2,17 +2,9 @@ package main
 
 import (
 	"log"
-	"net/http"
 
 	"github.com/cr1m3s/tch_backend/configs"
-	"github.com/cr1m3s/tch_backend/controllers"
-	_ "github.com/cr1m3s/tch_backend/docs"
-	middleware "github.com/cr1m3s/tch_backend/middlewares"
-	"github.com/cr1m3s/tch_backend/models"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/cr1m3s/tch_backend/routers"
 )
 
 // @title						Study marketplace API
@@ -31,47 +23,9 @@ import (
 // @name						Authorization
 func main() {
 	configs.LoadAndCheck()
-	server := gin.Default()
-	sorsConfig := cors.DefaultConfig()
-	sorsConfig.AddAllowHeaders("Access-Control-Allow-Headers")
-	sorsConfig.AddAllowHeaders("Access-Control-Request-Method")
-	sorsConfig.AddAllowHeaders("Access-Control-Request-Headers")
-	sorsConfig.AddAllowHeaders("X-Requested-With")
-	sorsConfig.AddAllowHeaders("Accept")
-	sorsConfig.AddAllowHeaders("Authorization")
-	sorsConfig.AllowAllOrigins = true
-	sorsConfig.AllowCredentials = true
-	c := cors.New(sorsConfig)
-	server.Use(c)
-
-	// router.POST("/register", controllers.Register)
-	// localhost gonna be used by default
-	AuthController := controllers.NewUsersController()
-	AuthGoogleController := controllers.NewAuthGoogleController()
-
-	router := server.Group("/api")
-	router.GET("/", HealthCheck)
-	url := ginSwagger.URL(configs.DOCS_HOSTNAME + "/api/docs/doc.json")
-	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
-	router.POST("/auth/register", AuthController.UserRegister)
-	router.POST("/auth/login", AuthController.UserLogin)
-	router.GET("/auth/login-google", AuthGoogleController.LoginGoogle)
-	router.GET("/auth/login-google-callback", AuthGoogleController.LoginGoogleCallback)
-	protected := server.Group("/protected")
-	protected.Use(middleware.AuthMiddleware())
-	protected.GET("/userinfo", AuthController.UserInfo)
+	server := routers.SetupRouter()
+	routers.SetupCORS(server)
 
 	log.Fatal(server.Run(configs.SERVER_HOSTNAME))
 }
 
-// HealthCheck godoc
-// @Summary		Show the status of server.
-// @Description	get the status of server.
-// @Tags			root
-// @Accept			*/*
-// @Produce		json
-// @Success		200	{object}	map[string]interface{}
-// @Router			/api/ [get]
-func HealthCheck(c *gin.Context) {
-	c.JSON(http.StatusOK, models.NewResponseSuccess("Server up and running."))
-}
---
+++ routers/routers.go
@@ -0,0 +1,53 @@
+package routers
+
+import (
+
+	"github.com/cr1m3s/tch_backend/configs"
+	"github.com/cr1m3s/tch_backend/controllers"
+	_ "github.com/cr1m3s/tch_backend/docs"
+	middleware "github.com/cr1m3s/tch_backend/middlewares"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func SetupRouter() *gin.Engine {
+
+	AuthController := controllers.NewUsersController()
+	AuthGoogleController := controllers.NewAuthGoogleController()
+	docs_url := ginSwagger.URL(configs.DOCS_HOSTNAME + "/api/docs/doc.json")
+	
+	server := gin.Default()
+	api := server.Group("/api")
+
+	api.GET("/", controllers.HealthCheck)
+	api.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, docs_url))
+	api.POST("/auth/register", AuthController.UserRegister)
+	api.POST("/auth/login", AuthController.UserLogin)
+	api.GET("/auth/login-google", AuthGoogleController.LoginGoogle)
+	api.GET("/auth/login-google-callback", AuthGoogleController.LoginGoogleCallback)
+
+	protected := server.Group("/protected")
+	
+	protected.Use(middleware.AuthMiddleware())
+	protected.GET("/userinfo", AuthController.UserInfo)
+
+	return server
+}
+
+func SetupCORS(r *gin.Engine) {
+
+	sorsConfig := cors.DefaultConfig()
+	sorsConfig.AddAllowHeaders("Access-Control-Allow-Headers")
+	sorsConfig.AddAllowHeaders("Access-Control-Request-Method")
+	sorsConfig.AddAllowHeaders("Access-Control-Request-Headers")
+	sorsConfig.AddAllowHeaders("X-Requested-With")
+	sorsConfig.AddAllowHeaders("Accept")
+	sorsConfig.AddAllowHeaders("Authorization")
+	sorsConfig.AllowAllOrigins = true
+	sorsConfig.AllowCredentials = true
+	c := cors.New(sorsConfig)
+	
+	r.Use(c)
+}
