20b17c674ae27b39a4f69e6870cc2f547505a525
cr1m3s <parioleks@gmail.com>
2023-11-04 18:44:17 +0100

    removed creation time from adv update model

--- models/advertisements.go
+++ models/advertisements.go
@@ -1,7 +1,5 @@
 package models
 
-import "time"
-
 type AdvertisementInput struct {
 	Title       string `json:"title"`
 	Attachment  string `json:"attachment"`
@@ -17,19 +15,18 @@ type AdvertisementInput struct {
 }
 
 type AdvertisementUpdate struct {
-	ID          int64     `json:"id"`
-	Title       string    `json:"title"`
-	CreatedAt   time.Time `json:"created_at"`
-	Attachment  string    `json:"attachment"`
-	Experience  string    `json:"experience"`
-	Category    string    `json:"category"`
-	Time        string    `json:"time"`
-	Price       int32     `json:"price"`
-	Format      string    `json:"format"`
-	Language    string    `json:"language"`
-	Description string    `json:"description"`
-	MobilePhone string    `json:"mobile_phone"`
-	Telegram    string    `json:"telegram"`
+	ID          int64  `json:"id"`
+	Title       string `json:"title"`
+	Attachment  string `json:"attachment"`
+	Experience  string `json:"experience"`
+	Category    string `json:"category"`
+	Time        string `json:"time"`
+	Price       int32  `json:"price"`
+	Format      string `json:"format"`
+	Language    string `json:"language"`
+	Description string `json:"description"`
+	MobilePhone string `json:"mobile_phone"`
+	Telegram    string `json:"telegram"`
 }
 
 type AdvertisementDelete struct {
