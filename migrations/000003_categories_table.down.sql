25797dd6e6c6b7af64b316f700cdb4b7a0c8a1b9
cr1m3s <parioleks@gmail.com>
2023-11-19 18:32:45 +0100

    README update

--- migrations/000003_categories_table.down.sql
+++ migrations/000003_categories_table.down.sql
@@ -1,6 +1,1 @@
-CREATE TABLE categories (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR NOT NULL,
-    parent_id INT,
-    FOREIGN KEY (parent_id) REFERENCES categories(id)
-);
+DROP TABLE IF EXISTS categories;
